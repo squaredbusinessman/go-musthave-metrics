5fb14f328b9fa7176e6ee799b6599a67db341761
EVGENY ANTROPOV <burdaonelove@gmail.com>
2025-11-24 13:55:38 +0300

    feat: Реализуем 2 новые функции пакета agent - sendMetric и reportMetrics, для единичной и полноценной фиксации метрик на сервере.

---
+++ internal/agent/report_metrics.go
@@ -0,0 +1,34 @@
+package agent
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	storage "github.com/squaredbusinessman/go-musthave-metrics/internal/repository"
+)
+
+// reportMetrics функция отправки всех фиксируемых метрик
+func reportMetrics(client *http.Client, store *storage.MemStorage, serverAddr string) {
+	gauges, counters := store.SnapShot()
+	for name, value := range gauges {
+		if err := sendMetric(
+			client,
+			serverAddr,
+			"gauge",
+			name,
+			strconv.FormatFloat(value.Value, 'f', -1, 64)); err != nil {
+			log.Printf("Failed to send gauge: %s", err)
+		}
+	}
+
+	for name, value := range counters {
+		if err := sendMetric(
+			client,
+			serverAddr,
+			"counter",
+			name, strconv.FormatInt(value.Value, 10)); err != nil {
+			log.Printf("Failed to send counter: %s", err)
+		}
+	}
+}
--- internal/agent/send_metric.go
+++ internal/agent/send_metric.go
@@ -1,1 +1,29 @@
 package agent
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// sendMetric Функция отправки ОДНОЙ метрики
+func sendMetric(client *http.Client, serverAddr string, metricType string, metricName string, metricValue string) error {
+	url := fmt.Sprintf("http://%s/update/%s/%s/%s", serverAddr, metricType, metricName, metricValue)
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "text/plain")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	return nil
+}
