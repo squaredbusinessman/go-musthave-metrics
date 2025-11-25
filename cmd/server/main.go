52abdfa98dd8eb4c27fb12266c53b72d2be99417
EVGENY ANTROPOV <burdaonelove@gmail.com>
2025-11-25 10:05:31 +0300

    feat: Добавляет использование chi вместо стандартного пакета net/http

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/squaredbusinessman/go-musthave-metrics/internal/handler"
 	storage "github.com/squaredbusinessman/go-musthave-metrics/internal/repository"
 )
@@ -12,11 +13,13 @@ func main() {
 	// Создаём экземпляр хранилища
 	metricsStorage := storage.NewMemStorage()
 
+	r := chi.NewRouter()
+
 	// Регистрируем обработчик из пакета handler с передачей хранилища
-	http.HandleFunc("/update/", handler.AcceptMetricsToStorage(metricsStorage))
+	r.Post("/update/", handler.AcceptMetricsToStorage(metricsStorage))
 
 	log.Println("Listening on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("could not start server: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/squaredbusinessman/go-musthave-metrics
 
 go 1.23.3
+
+require github.com/go-chi/chi/v5 v5.2.3
