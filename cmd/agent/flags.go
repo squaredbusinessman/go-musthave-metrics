f4d2925e34a84b013f760415c4e227f8f98c7be2
EVGENY ANTROPOV <burdaonelove@gmail.com>
2026-02-02 14:56:02 +0300

    feat: добавляет флаг RateLimit  в конфиг агента, чтобы потом с помощью него ограничивать работу воркерпула в main агента

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -15,6 +15,8 @@ type Config struct {
 	PollInterval   int    `env:"POLL_INTERVAL"`
 	ReportInterval int    `env:"REPORT_INTERVAL"`
 	ReportFormat   string `env:"REPORT_FORMAT"`
+	Key            string `env:"KEY"`
+	RateLimit      int    `env:"RATE_LIMIT"`
 }
 
 func parseConfig() Config {
@@ -24,6 +26,8 @@ func parseConfig() Config {
 	flag.IntVar(&cfg.PollInterval, "p", 2, "Poll interval(seconds)")
 	flag.IntVar(&cfg.ReportInterval, "r", 10, "Report interval(seconds)")
 	flag.StringVar(&cfg.ReportFormat, "f", agent.ReportFormatPlain, "Report format: plain or json")
+	flag.StringVar(&cfg.Key, "k", "", "Hash key")
+	flag.IntVar(&cfg.RateLimit, "l", 1, "Rate limiting")
 
 	flag.Parse()
 
